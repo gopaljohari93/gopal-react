009e42ddfb2b98d6c24669727db49e5872a6072e
joharigopal5 <joharigopal5@gmail.com>
2024-01-09 17:35:59 +0530

    add project file

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -2,7 +2,20 @@
 
 function App() {
   return (
-   <h1>Learn React | Gopal Johari </h1>
+   <h1>Learn React | Gopal Johari 
+    Create react projects - 1 learn git repository and push code github
+						  2 Create React App 1 project - 
+										  npx create-react-app gopal-react
+											  npm run start
+											  npm run build
+						  3 Create React App 2 project - gopal-react
+										 npm create vite@latest
+										 √ Project name: ... 01vitereact
+										 √ Select a framework: » React
+										 √ Select a variant: » JavaScript
+										 install - npm i
+										 npm run dev
+   </h1>
   );
 }
 
