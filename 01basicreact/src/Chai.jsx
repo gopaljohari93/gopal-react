10b5383da790e23671c2cf7e202efd74d4d456d2
joharigopal5 <joharigopal5@gmail.com>
2024-01-10 14:02:06 +0530

    Understand the react flow and structure

---
+++ 01basicreact/src/Chai.jsx
@@ -0,0 +1,11 @@
+function Chai() {
+    return (
+        <>
+            <h1>chai in react</h1>
+            <p>jab app Component banare chahe vite se kam karo ya create-react-app se kam karo function component capital letter naam rakhna chayiye</p>
+            <h2>vite me file name Chai.jsx rakana chayiye</h2>
+            <h2>create-react-app me file name capital or Chai.jsx ya Chai.js rakh sakte hai</h2>
+        </>
+    )
+}
+export default Chai;
